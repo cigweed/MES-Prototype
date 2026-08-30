3844ac257382a467a821b2d87ca8094d9fa191cd
Kevin Chen <kevin6562378@gmail.com>
2026-08-30 23:33:13 +0800

    add department class

---
+++ src/main/java/com/example/test/Department.java
@@ -0,0 +1,18 @@
+package com.example.test;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="department")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer capacity;
+}
